18eecb432e7dc380a70505bdc0bda497d03213e7
Noemi Collantes <noemicollantereyes24@gmail.com>
2023-11-11 13:02:36 -0500

    Validaciones de Categoria

--- src/main/java/com/apiufpso/tienda/model/Category.java
+++ src/main/java/com/apiufpso/tienda/model/Category.java
@@ -1,9 +1,8 @@
 package com.apiufpso.tienda.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -11,7 +10,14 @@ import lombok.Data;
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long idCategory;
+
+    @NotNull(message = "nameCategory is required")
+    @Size(min=3, max = 150, message = "nameCategory min 3 characters and max 150")
     private String nameCategory;
+
+    @NotNull(message = "descriptionCategory is required")
+    @Size(min = 3, max = 255, message = "descriptionCategory min 3 characters and max 255")
     private String descriptionCategory;
 }
--- src/main/java/com/apiufpso/tienda/util/Constants.java
+++ src/main/java/com/apiufpso/tienda/util/Constants.java
@@ -1,11 +1,13 @@
 package com.apiufpso.tienda.util;
 
+import com.apiufpso.tienda.model.Category;
 import lombok.Getter;
 
 @Getter
 public enum Constants {
     USER_NOT_FOUND("User not found"),
-    ARTICLE_NOT_FOUND("Article not found");
+    ARTICLE_NOT_FOUND("Article not found"),
+    CATEGORY_NOT_FOUND("Category not found");
 
     private String message;
     Constants (String s){
